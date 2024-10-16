891ae56b80298423e9ab68c7f029d69bbb445200
Dmytro <dmalenik@gmail.com>
2024-10-16 14:10:30 +0300

    refactor: change CurrencyConverter component
    - remove semicolons
    - change list of currencies to fix error related with options tag

--- src/views/main/components/CurrencyConverter.tsx
+++ src/views/main/components/CurrencyConverter.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { LiaExchangeAltSolid } from 'react-icons/lia';
-import { SiQatarairways } from 'react-icons/si';
-import { GiEgyptianSphinx } from 'react-icons/gi';
-import { FaFlagUsa, FaLandmarkFlag } from 'react-icons/fa6';
-import { FaInfoCircle } from 'react-icons/fa';
-import { IoShieldCheckmark } from 'react-icons/io5';
+import React from 'react'
+import { LiaExchangeAltSolid } from 'react-icons/lia'
+import { SiQatarairways } from 'react-icons/si'
+import { GiEgyptianSphinx } from 'react-icons/gi'
+import { FaFlagUsa, FaLandmarkFlag } from 'react-icons/fa6'
+import { FaInfoCircle, FaSearch } from 'react-icons/fa'
+import { IoShieldCheckmark } from 'react-icons/io5'
 
 export const CurrencyConverter = () => {
   return (
@@ -16,57 +16,74 @@ export const CurrencyConverter = () => {
           currency converter calculator.
         </p>
       </header>
-      <form action="">
-        <div className="inputs">
-          <div className="input-card">
-            <div className="input">
-              <label htmlFor="send-amount">Send Amount</label>
-              <input type="number" id="send-amount" />
+      <form action=''>
+        <div className='currency-converter'>
+          <div className='card'>
+            <div className='currency-value'>
+              <label htmlFor='send'>Send Amount</label>
+              <input type='number' id='send' />
             </div>
-            <div>
+            <div className='current-currency'>
               <SiQatarairways />
               <p>QAR</p>
             </div>
           </div>
-          <span className="arrows-icon">
+          <span className='arrows-icon'>
             <LiaExchangeAltSolid />
           </span>
-          <div className="input-card">
-            <div className="input">
-              <label htmlFor="receive-amount">Receiver gets</label>
-              <input type="number" id="receive-amount" />
-              <select
-                id="currencies-list"
-                aria-label="Choose a currency from the list"
-              >
-                <option>
-                  <div className="currency">
-                    <GiEgyptianSphinx />
-                    <p>EGP</p>
-                  </div>
-                </option>
-                <option>
-                  <div className="currency">
-                    <FaFlagUsa />
-                    <p>USD</p>
-                  </div>
-                </option>
-                <option>
-                  <div className="currency">
-                    <FaLandmarkFlag />
-                    <p>UAH</p>
-                  </div>
-                </option>
-              </select>
+          <div className='card'>
+            <div className='currency-value'>
+              <label htmlFor='receive'>Receiver gets</label>
+              <input type='number' id='receive' />
+            </div>
+            <div>
+              {/** Currently selected currency*/}
+              <div className='current-currency'>
+                <GiEgyptianSphinx />
+                <p>EGP</p>
+              </div>
+              <div className='currencies-list'>
+                {/** Is hidden*/}
+                <div>
+                  <label htmlFor='currency-search'>
+                    <FaSearch />
+                  </label>
+                  <input
+                    type='search'
+                    id='currency-search'
+                    placeholder='Search'
+                  />
+                </div>
+                <ul>
+                  <li>
+                    <div>
+                      <GiEgyptianSphinx />
+                      <p>EGP</p>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <FaFlagUsa />
+                      <p>USD</p>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <FaLandmarkFlag />
+                      <p>UAH</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-        <div className="info">
+        <div className='info'>
           <p>FX: 1.00 QAR = 0.2693 USD</p>
           <p>Fee: 30.00 QAR</p>
-          <div className="info-container">
+          <div>
             <FaInfoCircle />
-            <p className="info-text">
+            <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum et
               quo ea culpa vitae, incidunt necessitatibus in voluptates,
               accusantium accusamus voluptatibus sunt libero fugit, odit
@@ -74,7 +91,7 @@ export const CurrencyConverter = () => {
             </p>
           </div>
         </div>
-        <button type="button">Send money</button>
+        <button type='button'>Send money</button>
       </form>
       <div>
         <IoShieldCheckmark />
@@ -85,5 +102,5 @@ export const CurrencyConverter = () => {
         </p>
       </div>
     </section>
-  );
-};
+  )
+}
