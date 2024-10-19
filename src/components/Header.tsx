bb626b02bc4080e1ae81420f59602899a75667d5
Dmytro <dmalenik@gmail.com>
2024-10-19 15:28:47 +0300

    feat: add conditional to description

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = (props: any) => {
   return (
     <header>
       <h1>{props.title}</h1>
-      <p>{props.description}</p>
+      {props.description ? <p>{props.description}</p> : undefined}
     </header>
   )
 }
