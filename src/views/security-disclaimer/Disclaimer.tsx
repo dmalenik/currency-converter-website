4f0ac85154eb53656b4a9ae9b647766a6590aae4
Dmytro <dmalenik@gmail.com>
2024-10-19 13:40:00 +0300

    refactor: extract Disclaimer component

---
+++ src/views/security-disclaimer/Disclaimer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Disclaimer = (props: any) => {
+  return (
+    <div>
+      {props.icon}
+      <p>{props.description}</p>
+    </div>
+  )
+}
--- src/views/security-disclaimer/SecurityDisclaimer.tsx
+++ src/views/security-disclaimer/SecurityDisclaimer.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { FaUnlockAlt } from 'react-icons/fa';
-import { FaListCheck } from 'react-icons/fa6';
+import React from 'react'
+import { FaUnlockAlt } from 'react-icons/fa'
+import { FaListCheck } from 'react-icons/fa6'
+import { Disclaimer } from './Disclaimer'
 
 export const SecurityDisclaimer = () => {
   return (
     <section>
-      <div>
-        <FaUnlockAlt />
-        <p>We encrypt your transfers.</p>
-      </div>
-      <div>
-        <FaListCheck />
-        <p>We are committed to keeping your data secure.</p>
-      </div>
+      <Disclaimer
+        icon={<FaUnlockAlt />}
+        description={'We encrypt your transfers.'}
+      />
+      <Disclaimer
+        icon={<FaListCheck />}
+        description={'We are committed to keeping your data secure.'}
+      />
     </section>
-  );
-};
+  )
+}
