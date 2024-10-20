b8607500a8808352c2838ec54a08068ff4f15039
Dmytro <dmalenik@gmail.com>
2024-10-20 11:26:43 +0300

    refactor: implement SecurityDisclaimer in DisclaimerSection

--- src/views/disclaimer/DisclaimerSection.tsx
+++ src/views/disclaimer/DisclaimerSection.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
+import React from 'react'
+import { SecurityDisclaimer } from '../../components'
 
 export const DisclaimerSection = () => {
   return (
     <section>
-      <p>
-        * Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
-        voluptatem et repellat nihil, necessitatibus impedit, pariatur ratione
-        distinctio dolores omnis totam tempore blanditiis ipsa! Cumque quis
-        quasi vel dolores, autem nisi sit eligendi eveniet tempora nobis fugit
-        rerum voluptatibus eos totam assumenda, laboriosam vero omnis at in.
-        Tenetur itaque deserunt similique obcaecati illo repudiandae ea et ut
-        nulla natus, nesciunt necessitatibus distinctio quam fugiat repellendus
-        earum dolor sapiente aperiam! Nulla, cum voluptate. Reprehenderit,
-        maiores similique.
-      </p>
+      <SecurityDisclaimer
+        description={
+          '* Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisvoluptatem et repellat nihil, necessitatibus impedit, pariatur rationedistinctio dolores omnis totam tempore blanditiis ipsa! Cumque quisquasi vel dolores, autem nisi sit eligendi eveniet tempora nobis fugitrerum voluptatibus eos totam assumenda, laboriosam vero omnis at in.Tenetur itaque deserunt similique obcaecati illo repudiandae ea et utnulla natus, nesciunt necessitatibus distinctio quam fugiat repellendusearum dolor sapiente aperiam! Nulla, cum voluptate. Reprehenderit,maiores similique.'
+        }
+      />
     </section>
-  );
-};
+  )
+}
