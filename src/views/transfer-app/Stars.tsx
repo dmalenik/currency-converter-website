88144dfe4f756ea458a130b316d25fad7e28bd8a
Dmytro <dmalenik@gmail.com>
2024-10-20 10:16:45 +0300

    refactor: extract Stars component

---
+++ src/views/transfer-app/Stars.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaRegStar } from 'react-icons/fa6'
+
+import { type StarsProps } from './starsProps'
+
+export const Stars = (props: StarsProps) => {
+  // TODO: Implement OWN fractional rating system
+  return (
+    <div>
+      {Array.from({ length: props.stars }, (_, i) => (
+        <FaRegStar
+          key={crypto.randomUUID()}
+          color={i < props.rating ? 'gold' : 'grey'}
+        />
+      ))}
+    </div>
+  )
+}
---
+++ src/views/transfer-app/starsProps.ts
@@ -0,0 +1,4 @@
+export type StarsProps = {
+  stars: number
+  rating: number
+}
