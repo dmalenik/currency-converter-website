51ef6684b419d7e39748d77e4b54329f2bfac22f
Dmytro <dmalenik@gmail.com>
2024-10-28 20:49:55 +0100

    refactor: change UnorderedList in Transfer

--- src/views/transfer/Transfer.tsx
+++ src/views/transfer/Transfer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, List, Action } from '../../components'
+import { Header, UnorderedList, Action, Card } from '../../components'
 import { FaHeart, FaShieldAlt } from 'react-icons/fa'
 import { LuClock4 } from 'react-icons/lu'
 import { FaGlobe } from 'react-icons/fa6'
@@ -12,69 +12,65 @@ export const Transfer = () => {
           "Our customers made millons of transfers with Western Union last year. Here's why:"
         }
       />
-      <List
-        list={[
-          {
-            id: '0',
-            icon: <FaHeart />,
-            title: 'Ease and confidence',
-            description:
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velitcum ab repellendus doloribus fugiat mollitia.',
-          },
-          {
-            id: '1',
-            icon: <FaShieldAlt />,
-            title: 'Commitment to security',
-            description:
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti neque magnam dolores.',
-          },
-          {
-            id: '2',
-            icon: <LuClock4 />,
-            title: 'Spread and transparency',
-            description:
-              'Lorem ipsum dolor sit amet consectetur* adipisicing elit. Architecto assumenda totam tempore velit. Pariatur, nisi?',
-          },
-          {
-            id: '3',
-            icon: <FaGlobe />,
-            title: 'Worldwide reach',
-            description:
-              'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus eligendi odit blanditiis saepe nobis optio vel laborumatque quos autem!',
-          },
-        ]}
-      />
+      <UnorderedList>
+        <Card
+          icon={<FaHeart />}
+          title={'Ease and confidence'}
+          description={
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velitcum ab repellendus doloribus fugiat mollitia.'
+          }
+        />
+        <Card
+          icon={<FaShieldAlt />}
+          title={'Commitment to security'}
+          description={
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti neque magnam dolores.'
+          }
+        />
+        <Card
+          icon={<LuClock4 />}
+          title={'Spread and transparency'}
+          description={
+            'Lorem ipsum dolor sit amet consectetur* adipisicing elit. Architecto assumenda totam tempore velit. Pariatur, nisi?'
+          }
+        />
+        <Card
+          icon={<FaGlobe />}
+          title={'Worldwide reach'}
+          description={
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus eligendi odit blanditiis saepe nobis optio vel laborumatque quos autem!'
+          }
+        />
+      </UnorderedList>
       <section>
         <Header title={'Register to start sending money today.'} />
         <Action action={'Register now'} />
-        <List
-          list={[
-            {
-              id: '0',
-              listHeading: 'Quick and easy registration',
-              description:
-                'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natuscorrupti tempore nobis quis!',
-            },
-            {
-              id: '1',
-              listHeading: 'Pay the way you like',
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasquasi blanditiis voluptatibus libero consequuntur quod.',
-            },
-            {
-              id: '2',
-              listHeading: 'Track your transfer',
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitisdolorem quidem velit.',
-            },
-            {
-              id: '3',
-              listHeading: 'Convenient locations',
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepefacilis, delectus sunt natus earum mollitia dolore beatae optioincidunt rerum laborum similique reprehenderit accusamusrepellat?',
-            },
-          ]}
-        />
+        <UnorderedList>
+          <Card
+            title={'Quick and easy registration'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natuscorrupti tempore nobis quis!'
+            }
+          />
+          <Card
+            title={'Pay the way you like'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasquasi blanditiis voluptatibus libero consequuntur quod.'
+            }
+          />
+          <Card
+            title={'Track your transfer'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitisdolorem quidem velit.'
+            }
+          />
+          <Card
+            title={'Convenient locations'}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepefacilis, delectus sunt natus earum mollitia dolore beatae optioincidunt rerum laborum similique reprehenderit accusamusrepellat?'
+            }
+          />
+        </UnorderedList>
       </section>
       <section>
         <Header title={'Lorem ipsum dolor sit amet consectetur adipisicing.'} />
