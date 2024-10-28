d271459d5a17ea7e2a2a02037643a2c39bf0aa1b
Dmytro <dmalenik@gmail.com>
2024-10-28 22:57:16 +0100

    refactor: implement Search, Header, and UnorderedList components

--- src/views/map/Map.tsx
+++ src/views/map/Map.tsx
@@ -1,168 +1,76 @@
-import React from 'react';
-import { SiQatarairways } from 'react-icons/si';
-import {
-  GiGrainBundle,
-  GiEgyptianSphinx,
-  GiSpain,
-  GiIndianPalace,
-  GiAfrica,
-} from 'react-icons/gi';
-import { FaFlagUsa } from 'react-icons/fa6';
-import { FaApple, FaGooglePlay } from 'react-icons/fa';
+import React from 'react'
+import { SiQatarairways } from 'react-icons/si'
+import { GiGrainBundle, GiEgyptianSphinx, GiSpain } from 'react-icons/gi'
+import { FaFlagUsa } from 'react-icons/fa6'
+import { Search, Link, Markets, UnorderedList, Header } from '../../components'
 
 export const Map = () => {
   return (
     <section>
       <div>
-        <ul>
-          <li>
-            <div>
-              <select
-                name="countriesList"
-                id="countries-list"
-                aria-label="Choose a country from a list"
-              >
-                <option value="Qatar">
-                  <div>
-                    <SiQatarairways />
-                    <p>Qatar</p>
-                  </div>
-                </option>
-                <option value="Ukraine">
-                  <div>
-                    <GiGrainBundle />
-                    <p>Ukraine</p>
-                  </div>
-                </option>
-                <option value="USA">
-                  <div>
-                    <FaFlagUsa />
-                    <p>USA</p>
-                  </div>
-                </option>
-              </select>
-              <section>
-                <h3>Money transfer</h3>
-                <ul>
-                  <li>
-                    <button type="button">Lorem ipsum dolor sit amet.</button>
-                  </li>
-                  <li>
-                    <button type="button">Lorem ipsum dolor sit amet.</button>
-                  </li>
-                  <li>
-                    <button type="button">Lorem ipsum dolor sit amet.</button>
-                  </li>
-                  <li>
-                    <button type="button">Lorem ipsum dolor sit amet.</button>
-                  </li>
-                  <li>
-                    <button type="button">Lorem ipsum dolor sit amet.</button>
-                  </li>
-                </ul>
-              </section>
-            </div>
-          </li>
-          <li>
-            <section>
-              <h3>Support</h3>
-              <ul>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-              </ul>
-            </section>
-          </li>
-          <li>
-            <section>
-              <h3>Quick links</h3>
-              <ul>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-              </ul>
-            </section>
-          </li>
-          <li>
-            <section>
-              <h3>Legal</h3>
-              <ul>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-                <li>
-                  <button type="button">Lorem ipsum dolor sit amet.</button>
-                </li>
-              </ul>
-            </section>
-          </li>
-        </ul>
+        <Search
+          items={[
+            <Link icon={<SiQatarairways />} description={'Qatar'} />,
+            <Link icon={<GiGrainBundle />} description={'Ukraine'} />,
+            <Link icon={<FaFlagUsa />} description={'USA'} />,
+          ]}
+        />
+        <section>
+          <Header title={'Money transfer'} />
+          <UnorderedList>
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+          </UnorderedList>
+        </section>
+        <section>
+          <Header title={'Support'} />
+          <UnorderedList>
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+          </UnorderedList>
+        </section>
+        <section>
+          <Header title={'Quick links'} />
+          <UnorderedList>
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+          </UnorderedList>
+        </section>
+        <section>
+          <Header title={'Legal'} />
+          <UnorderedList>
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+            <Link description={'Lorem ipsum dolor sit amet.'} />
+          </UnorderedList>
+        </section>
       </div>
       <div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
           ad minima suscipit reiciendis aut hic minus recusandae!
         </p>
-        <div>
-          <div>
-            <GiEgyptianSphinx />
-            <p>Egypt</p>
-          </div>
-          <div>
-            <GiSpain />
-            <p>Philippines</p>
-          </div>
-          <div>
-            <GiIndianPalace />
-            <p>India</p>
-          </div>
-          <div>
-            <GiAfrica />
-            <p>Lebanon</p>
-          </div>
-          <div>
-            <GiAfrica />
-            <p>Tunisia</p>
-          </div>
-        </div>
-        <div>
-          <FaApple />
-          <FaGooglePlay />
-        </div>
+        <UnorderedList>
+          <Link icon={<GiEgyptianSphinx />} description={'Egypt'} />
+          <Link icon={<GiSpain />} description={'Phillippines'} />
+          <Link icon={<GiEgyptianSphinx />} description={'India'} />
+          <Link icon={<GiEgyptianSphinx />} description={'Lebanon'} />
+          <Link icon={<GiEgyptianSphinx />} description={'Tunisia'} />
+        </UnorderedList>
+        <Markets />
       </div>
     </section>
-  );
-};
+  )
+}
