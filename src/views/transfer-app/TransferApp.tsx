9e32b0d20a71761a562f3dd355383400984f7252
Dmytro <dmalenik@gmail.com>
2024-10-28 23:01:40 +0100

    refactor: apply heading tag and implement UnorderedList compontn

--- src/views/transfer-app/TransferApp.tsx
+++ src/views/transfer-app/TransferApp.tsx
@@ -1,40 +1,30 @@
 import React from 'react'
-import { FaCheckCircle, FaApple, FaGooglePlay } from 'react-icons/fa'
+import { FaCheckCircle } from 'react-icons/fa'
 import { IoMdPhonePortrait } from 'react-icons/io'
-import { Header, List, Link } from '../../components'
+import { Header, UnorderedList, Link, Card, Markets } from '../../components'
 import { Rating } from './Rating'
 
 export const TransferApp = () => {
   return (
     <section>
       <div className='app'>
-        <Header
-          title={
-            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos.'
-          }
-        />
-        <List
-          list={[
-            {
-              id: '1',
-              icon: <FaCheckCircle />,
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quovitae inventore nulla sequi!',
-            },
-            {
-              id: '2',
-              icon: <FaCheckCircle />,
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing.',
-            },
-            {
-              id: '3',
-              icon: <FaCheckCircle />,
-              description:
-                'Lorem ipsum dolor sit amet consectetur adipisicing.',
-            },
-          ]}
-        />
+        <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos.</h2>
+        <UnorderedList>
+          <Card
+            icon={<FaCheckCircle />}
+            description={
+              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quovitae inventore nulla sequi!'
+            }
+          />
+          <Card
+            icon={<FaCheckCircle />}
+            description={'Lorem ipsum dolor sit amet consectetur adipisicing.'}
+          />
+          <Card
+            icon={<FaCheckCircle />}
+            description={'Lorem ipsum dolor sit amet consectetur adipisicing.'}
+          />
+        </UnorderedList>
         <Link description={'Download now'} />
         <div className='downloads'>
           <div className='rating'>
@@ -42,16 +32,7 @@ export const TransferApp = () => {
             <Rating rating={4} />
             <p>Lorem ipsum dolor sit amet consectetur.</p>
           </div>
-          <div className='markets'>
-            <Link
-              description={<FaApple />}
-              ariaLabel={'Download app in App Store'}
-            />
-            <Link
-              description={<FaGooglePlay />}
-              ariaLabel={'Download app in Google Play Store'}
-            />
-          </div>
+          <Markets />
         </div>
       </div>
       <div className='banner'>
