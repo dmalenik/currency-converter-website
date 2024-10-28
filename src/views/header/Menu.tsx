30acf7ef6c923e565c90bbcd148548aa7c3bead7
Dmytro <dmalenik@gmail.com>
2024-10-28 19:29:19 +0100

    refactor: implement UnorderedList in Menu

--- src/views/header/Menu.tsx
+++ src/views/header/Menu.tsx
@@ -1,31 +1,19 @@
 import React from 'react'
-import { Link } from '../../components'
+import { Link, UnorderedList } from '../../components'
 import { CiMenuBurger } from 'react-icons/ci'
 import { Language } from './Language'
 
 export const Menu = () => {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link description={'Send money'} />
-        </li>
-        <li>
-          <Link description={'Track a transfer'} />
-        </li>
-        <li>
-          <CiMenuBurger />
-        </li>
-        <li>
-          <Language />
-        </li>
-        <li>
-          <Link description={'Register'} />
-        </li>
-        <li>
-          <Link description={'Log In'} />
-        </li>
-      </ul>
+      <UnorderedList>
+        <Link description={'Send money'} />
+        <Link description={'Track a transfer'} />
+        <CiMenuBurger />
+        <Language />
+        <Link description={'Register'} />
+        <Link description={'Log In'} />
+      </UnorderedList>
     </nav>
   )
 }
