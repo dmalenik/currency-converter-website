70ba8a7f7e7e2178d2f618bfbb6ddf2e3ecd3132
Dmytro <dmalenik@gmail.com>
2024-10-30 13:56:38 +0100

    refactor: replace code in CompanyAddress with Logo

--- src/views/footer-section/CompanyAddress.tsx
+++ src/views/footer-section/CompanyAddress.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
-import { MdAnchor } from 'react-icons/md'
+import { Logo } from '../../components'
 
 export const CompanyAddress = () => {
   return (
     <div>
-      {/** Address */}
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aliquid
         eius commodi beatae totam, ullam id error assumenda.
       </p>
-      {/** Logo */}
-      <div>
-        <MdAnchor />
-        <p>Logo</p>
-      </div>
+      <Logo />
     </div>
   )
 }
--- src/views/header-section/Header.tsx
+++ src/views/header-section/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Logo } from './Logo'
+import { Logo } from '../../components'
 import { Menu } from './Menu'
 
 export const Header = () => {
