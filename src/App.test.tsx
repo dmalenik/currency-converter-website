148bcbd631bc7a88d46a28509e5c0876912c2bc0
Dmytro <dmalenik@gmail.com>
2024-11-06 13:52:11 +0100

    test(App): add todos

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
+
 test('renders app', () => {
   render(<App />)
+  // TODO: app is presented in the document
   const app = screen.getByTestId('app')
   expect(app).toBeInTheDocument()
+  // TODO: app includes header section
+  // TODO: app includes main section
+  // TODO: app includes footer section
 })
