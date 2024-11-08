ae8cf8da5d789db0d4da8908591672b18bd706b9
Dmytro <dmalenik@gmail.com>
2024-11-08 14:03:13 +0100

    test(Card): implement tests

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-import {type CardProps } from '../types'
+import { type CardProps } from '../types'
 
-export const Card = (props: CardProps) => {
+export const Card = ({ icon, title, description }: CardProps) => {
   return (
-    <div>
-      {props.icon}
-      <h4>{props.title}</h4>
-      <p>{props.description}</p>
+    <div data-testid='card'>
+      {icon ? <span data-testid='icon'>{icon}</span> : undefined}
+      <div>
+        {title ? <h4 data-testid='title'>{title}</h4> : null}
+        <p data-testid='description'>{description}</p>
+      </div>
     </div>
   )
 }
--- src/types/cardProps.ts
+++ src/types/cardProps.ts
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react'
 
 export type CardProps = {
-  id?: string
   icon?: ReactNode
   title?: string
   description: string
