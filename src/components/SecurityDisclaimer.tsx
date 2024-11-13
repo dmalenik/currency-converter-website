8144225affe0d8aa133e5ca5833287b067d7b84b
Dmytro <dmalenik@gmail.com>
2024-11-13 11:53:32 +0100

    test(SecurityDisclaimer): implement tests

--- src/components/SecurityDisclaimer.tsx
+++ src/components/SecurityDisclaimer.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import { type SecurityDisclaimerProps } from '../types'
 
-export const SecurityDisclaimer = (props: SecurityDisclaimerProps) => {
+export const SecurityDisclaimer = ({
+  icon,
+  description,
+}: SecurityDisclaimerProps) => {
   return (
-    <div>
-      {props.icon}
-      <p>{props.description}</p>
+    <div data-testid='security-disclaimer'>
+      {icon ? (
+        <div data-testid='security-disclaimer-icon'>{icon}</div>
+      ) : undefined}
+      <p data-testid='security-disclaimer-paragraph'>{description}</p>
     </div>
   )
 }
---
+++ src/test/SecurityDisclaimer.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { SecurityDisclaimer } from '../components'
+import { FaUnlockAlt } from 'react-icons/fa'
+
+test('SecurityDisclaimer renders in the document', () => {
+  render(
+    <SecurityDisclaimer
+      icon={<FaUnlockAlt />}
+      description={'We encrypt your transfers.'}
+    />,
+  )
+
+  const disclaimer = screen.getByTestId('security-disclaimer')
+
+  expect(disclaimer).toBeInTheDocument()
+})
+
+test('SecurityDisclaimer contains icon and paragraph', () => {
+  render(
+    <SecurityDisclaimer
+      icon={<FaUnlockAlt />}
+      description={'We encrypt your transfers.'}
+    />,
+  )
+
+  const disclaimer = screen.getByTestId('security-disclaimer')
+  const icon = screen.getByTestId('security-disclaimer-icon')
+  const paragraph = screen.getByTestId('security-disclaimer-paragraph')
+
+  expect(disclaimer).toContainElement(icon)
+  expect(disclaimer).toContainElement(paragraph)
+})
+
+// TODO: test SecurityDisclaimer contains only description and does not contains icon
+test('SecurityDisclaimer contains description and does not contains icon', () => {
+  render(<SecurityDisclaimer description={'We encrypt your transfers.'} />)
+
+  const disclaimer = screen.getByTestId('security-disclaimer')
+  const paragraph = screen.getByTestId('security-disclaimer-paragraph')
+  const icon = screen.queryByTestId('security-disclaimer-icon')
+
+  expect(disclaimer).toContainElement(paragraph)
+  expect(disclaimer).not.toContainElement(icon)
+})
