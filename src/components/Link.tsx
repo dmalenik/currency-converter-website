d97fe2177b6fc4e349af28876cf64c43a2c5ff72
Dmytro <dmalenik@gmail.com>
2024-11-13 12:05:00 +0100

    fix(Link): add conditionals to check if props are presented

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -6,8 +6,8 @@ export const Link = ({ icon, description, ariaLabel }: LinkProps) => {
   // TODO: replace button with a tag
   return (
     <button disabled type='button' aria-label={ariaLabel} data-testid='link'>
-      {icon}
-      {description}
+      {icon ? icon : undefined}
+      {description ? description : undefined}
     </button>
   )
 }
