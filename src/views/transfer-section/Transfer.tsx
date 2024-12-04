58afb372f1fe56319ff122b29ff6c4d19bb22dd8
Dmytro <dmalenik@gmail.com>
2024-12-04 11:26:14 +0300

    refactor: modify Transfer component

--- src/views/transfer-section/Transfer.tsx
+++ src/views/transfer-section/Transfer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Heading, UnorderedList, Action, Card } from '../../components'
+import { Heading, UnorderedList, Card } from '../../components'
 import { FaHeart, FaShieldAlt } from 'react-icons/fa'
 import { LuClock4 } from 'react-icons/lu'
 import { FaGlobe } from 'react-icons/fa6'
@@ -43,43 +43,6 @@ export const Transfer = () => {
           }
         />
       </UnorderedList>
-      <section>
-        <Heading title={'Register to start sending money today.'} level={4} />
-        <Action action={'Register now'} />
-        <UnorderedList>
-          <Card
-            title={'Quick and easy registration'}
-            description={
-              'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natuscorrupti tempore nobis quis!'
-            }
-          />
-          <Card
-            title={'Pay the way you like'}
-            description={
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasquasi blanditiis voluptatibus libero consequuntur quod.'
-            }
-          />
-          <Card
-            title={'Track your transfer'}
-            description={
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitisdolorem quidem velit.'
-            }
-          />
-          <Card
-            title={'Convenient locations'}
-            description={
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepefacilis, delectus sunt natus earum mollitia dolore beatae optioincidunt rerum laborum similique reprehenderit accusamusrepellat?'
-            }
-          />
-        </UnorderedList>
-      </section>
-      <section>
-        <Heading
-          title={'Lorem ipsum dolor sit amet consectetur adipisicing.'}
-          level={3}
-        />
-        <Action action={'Send money'} />
-      </section>
     </section>
   )
 }
