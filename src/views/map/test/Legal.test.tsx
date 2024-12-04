c0232000875ab27d8caaa2af2cbd648166d8fd4d
Dmytro <dmalenik@gmail.com>
2024-12-04 16:13:08 +0300

    refactor(Legal): optimize tests

--- src/views/map/test/Legal.test.tsx
+++ src/views/map/test/Legal.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { Legal } from '../components'
-import { UnorderedList, Link } from '../../../components'
 
 test('Legal is in the document', () => {
   render(<Legal />)
@@ -11,44 +10,13 @@ test('Legal is in the document', () => {
   expect(legal).toBeInTheDocument()
 })
 
-test('Legal contains heading and UnorderedList', () => {
+test('Legal contains UnorderedList', () => {
   render(<Legal />)
 
   const legal = screen.getByTestId('legal')
-  const title = screen.getByText('Legal')
-
-  expect(legal).toContainElement(title)
-})
-
-test('unordered list renders in the document', () => {
-  render(
-    <UnorderedList>
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-    </UnorderedList>,
-  )
-
   const unorderedList = screen.getByTestId('unordered-list')
-  const unorderedListChildren = screen.getAllByTestId('unordered-list-child')
+  const links = screen.queryAllByTestId('unordered-list-child')
 
-  expect(unorderedList).toBeInTheDocument()
-  expect(unorderedListChildren).toHaveLength(5)
+  expect(legal).toContainElement(unorderedList)
+  expect(links).toHaveLength(5)
 })
