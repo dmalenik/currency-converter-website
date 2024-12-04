7f20f3027693dce70f535da9c6bc31ce9c5cb6cb
Dmytro <dmalenik@gmail.com>
2024-12-04 16:10:15 +0300

    refactor(MoneyTransfer): optimize tests

--- src/views/map/test/MoneyTransfer.test.tsx
+++ src/views/map/test/MoneyTransfer.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { MoneyTransfer } from '../components'
-import { UnorderedList, Link } from '../../../components'
 
 test('MoneyTransfer renders in the document', () => {
   render(<MoneyTransfer />)
@@ -20,50 +19,4 @@ test('MoneyTransfer includes Search and money transfer section', () => {
 
   expect(moneyTransfer).toContainElement(search)
   expect(moneyTransfer).toContainElement(moneyTransferSection)
-})
-
-test('money transfer section includes heading and unordered list', () => {
-  render(<MoneyTransfer />)
-
-  const section = screen.getByTestId('money-transfer-section')
-  const heading = screen.getByText('Money transfer')
-
-  expect(section).toContainElement(heading)
-})
-
-test('UnorderedList renders in the document', () => {
-  render(
-    <UnorderedList>
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-      <Link
-        description={'Lorem ipsum dolor sit amet.'}
-        ariaLabel='lorem ipsum'
-      />
-    </UnorderedList>,
-  )
-
-  const unorderedList = screen.getByTestId('unordered-list')
-  const unorderedListChildren = screen.getAllByTestId('unordered-list-child')
-
-  expect(unorderedList).toBeInTheDocument()
-  expect(unorderedListChildren).toHaveLength(6)
 })
