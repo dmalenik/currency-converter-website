95fc6ed0e9497f503dc1583b5c2a7f3088407290
Dmytro <dmalenik@gmail.com>
2024-12-04 15:58:34 +0300

    refactor(Transfer): optimize tests

--- src/views/transfer-section/Transfer.test.tsx
+++ src/views/transfer-section/Transfer.test.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
 import { Transfer } from './Transfer'
-import { UnorderedList, Card } from '../../components'
-import { FaHeart, FaShieldAlt } from 'react-icons/fa'
-import { LuClock4 } from 'react-icons/lu'
-import { FaGlobe } from 'react-icons/fa6'
 
 test('Transfer redners in the document', () => {
   render(<Transfer />)
@@ -14,54 +10,13 @@ test('Transfer redners in the document', () => {
   expect(transfer).toBeInTheDocument()
 })
 
-test('Transfer includes Heading and UnorderedList', () => {
+test('Transfer includes UnorderedList', () => {
   render(<Transfer />)
 
   const transfer = screen.getByTestId('transfer')
-  const heading = screen.getByText(
-    "Our customers made millons of transfers with Western Union last year. Here's why:",
-  )
   const unorderedList = screen.getByTestId('unordered-list')
+  const cards = screen.queryAllByTestId('unordered-list-child')
 
-  expect(transfer).toContainElement(heading)
   expect(transfer).toContainElement(unorderedList)
-})
-
-test('UnorderedList has length of 4', () => {
-  render(
-    <UnorderedList>
-      <Card
-        icon={<FaHeart />}
-        title={'Ease and confidence'}
-        description={
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velitcum ab repellendus doloribus fugiat mollitia.'
-        }
-      />
-      <Card
-        icon={<FaShieldAlt />}
-        title={'Commitment to security'}
-        description={
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti neque magnam dolores.'
-        }
-      />
-      <Card
-        icon={<LuClock4 />}
-        title={'Spread and transparency'}
-        description={
-          'Lorem ipsum dolor sit amet consectetur* adipisicing elit. Architecto assumenda totam tempore velit. Pariatur, nisi?'
-        }
-      />
-      <Card
-        icon={<FaGlobe />}
-        title={'Worldwide reach'}
-        description={
-          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus eligendi odit blanditiis saepe nobis optio vel laborumatque quos autem!'
-        }
-      />
-    </UnorderedList>,
-  )
-
-  const cards = screen.queryAllByTestId('card')
-
   expect(cards).toHaveLength(4)
 })
